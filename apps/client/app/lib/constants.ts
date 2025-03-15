a130ece216ff79e950fafe21e53aedf5b89045a9
devMukulSingh <mukulsingh2276@gmail.com>
2025-03-15 09:53:56 +0530

    configured turebo.json for production

--- apps/client/app/lib/constants.ts
+++ apps/client/app/lib/constants.ts
@@ -1,14 +1,3 @@
 
-export const BASE_URL_SERVER = process.env.NODE_ENV==='production' ? "" : `http://localhost:8000/api/v1`
-
-export const ITEM_INITIAL_VALUES = {
-product:{
-    id:"",
-    rate:0,
-    name:""
-  },
-  product_id: '',
-  amount: 0,
-  quantity: 1,
-};
+export const BASE_URL_SERVER = process.env.NODE_ENV === 'production' ? "https://billmanagementserver.onrender.com" : `http://localhost:8000/api/v1`
 
