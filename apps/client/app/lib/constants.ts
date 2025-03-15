214e314043fe1273cba475b2b2a8006257387cbd
devMukulSingh <mukulsingh2276@gmail.com>
2025-03-15 09:58:21 +0530

    re added itemInitalValues

--- apps/client/app/lib/constants.ts
+++ apps/client/app/lib/constants.ts
@@ -1,3 +1,14 @@
 
 export const BASE_URL_SERVER = process.env.NODE_ENV === 'production' ? "https://billmanagementserver.onrender.com" : `http://localhost:8000/api/v1`
 
+export const ITEM_INITIAL_VALUES = {
+product:{
+    id:"",
+    rate:0,
+    name:""
+  },
+  product_id: '',
+  amount: 0,
+  quantity: 1,
+};
+
