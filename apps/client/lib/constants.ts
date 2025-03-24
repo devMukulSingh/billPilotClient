7582cd86fb124dba57e2661b9d398193f5d8c3bc
devMukulSingh <mukulsingh2276@gmail.com>
2025-03-24 12:11:56 +0530

    added base_url in .env

--- apps/client/lib/constants.ts
+++ apps/client/lib/constants.ts
@@ -1,5 +1,5 @@
 
-export const BASE_URL_SERVER = process.env.NODE_ENV === 'production' ? process.env.BASE_URL_SERVER : `http://localhost:8000/api/v1`
+export const BASE_URL_SERVER = process.env.BASE_URL_SERVER
 
 export const ITEM_INITIAL_VALUES = {
 product:{
@@ -11,5 +11,3 @@ product:{
   amount: 0,
   quantity: 1,
 };
-
-const arr = [].copyWithin
