5ae308a8dd03302c089b1856bfee5fe82807e815
devMukulSingh <mukulsingh2276@gmail.com>
2025-03-24 21:28:11 +0530

    fixed base url

--- apps/client/lib/constants.ts
+++ apps/client/lib/constants.ts
@@ -1,5 +1,5 @@
 
-export const BASE_URL_SERVER = process.env.NODE_ENV === 'production' ? `https://billmanagementserver.onrender.com` : `http://localhost:8000/api/v1`
+export const BASE_URL_SERVER = process.env.NODE_ENV === 'production' ? `https://billmanagementserver.onrender.com/api/v1` : `http://localhost:8000/api/v1`
 
 export const ITEM_INITIAL_VALUES = {
 product:{
